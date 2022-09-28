8eac51a9448fd44fefbd2b39badd8488998d0a8c
unknown <stefonplays@gmail.com>
2022-09-27 23:57:58 -0400

    Article creation, validation messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,30 @@ class ArticlesController < ApplicationController
 		@article = Article.find(@id)
 	end
 	
+	def index
+		@articles = Article.all
+	end
 
+	def new
+		@article = Article.new
+	end
+
+	def create
+		# Require article object form params, and permit title and description from this variable
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		if @article.save
+			# Create flash object so we can display it in view. Need to embed it in
+			# application.html.erb first
+			
+			flash[:notice] = "Article created successfully"
+			# Extract ID from article object and navigate to article_path/ID
+			# if we are able to create the article
+			redirect_to article_path(@article)
+		else
+			# Update for Rails 7, redirect to new page but now we have an article object
+			# with errors attributes to print out
+			render 'new', status: :unprocessable_entity
+		end
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
 	# Includes all necessary routes for articles controller, can
 	# exclude routes by using only: [:what_to_keep]
-	resources :articles, only: [:show]
+	resources :articles, only: [:show, :index, :new, :create]
 end
