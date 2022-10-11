2056d3cf3aad25548254c33771ec39f8e9853a78
unknown <stefonplays@gmail.com>
2022-10-10 21:08:01 -0400

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+	validates :username, presence: true, 
+											uniqueness: { case_sensitive: false }, 
+											length: { minimum: 3, maximum: 25 }
+	EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\.[a-z]+\z/
+	validates :email, presence: true, 
+											uniqueness: { case_sensitive: false }, 
+											length: { maximum: 105 },
+											format: { with: EMAIL_REGEX }
 
+	
 end
